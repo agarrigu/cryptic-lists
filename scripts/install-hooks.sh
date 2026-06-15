0b90a0387374522069abc91798512f4b4224f563
agarrigu <83713047+agarrigu@users.noreply.github.com>
2026-06-15 19:40:30 +0100

    scripts: add install-hooks script

---
+++ scripts/install-hooks.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+pushd .git/hooks
+if [[ ! -f pre-commit ]] ; then
+	touch pre-commit
+	echo '#!/usr/bin/env bash' > pre-commit
+	chmod +x scr
+fi
+
+# Do a for loop if there is more than one
+popd
+SORTTSVS_PATH=scripts/sort-tsvs.sh
+if [[ ! -f $SORTTSVS_PATH ]] ; then
+	echo "Script not found: $SORTTSVS_PATH" >&2
+	echo 'Are you sure you are running this in the projects root directory?' >&2
+	exit 1
+fi
+# We get from the second line to not include the shebang
+# If more files are included we should not assume it exists and check instead
+SORTTSVS=$(tail -n+2 $SORTTSVS_PATH)
+pushd .git/hooks
+echo "$SORTTSVS" >> pre-commit
+echo "Updated pre-commit hooks"
