d68823ac66f060d1e17e3776d134c412bd96694e
agarrigu <83713047+agarrigu@users.noreply.github.com>
2026-06-15 19:18:07 +0100

    scripts: sort-tsvs: expand error messages

--- sort-tsvs.sh
+++ sort-tsvs.sh
@@ -3,18 +3,22 @@
 TSVS=*.tsv
 HEADER_REGEX=$'^([A-Z-]{1,7}\.?\t)*[A-Z-]{1,7}\.?$'
 
-[[ -z $TSVS ]] && echo 'No .tsv files found' >&2 && exit 1
+if [[ -z $TSVS ]] ; then
+	echo 'No .tsv files found' >&2
+	echo 'Are you sure you are running this in the correct directory?' >&2
+	&& exit 1
 
 for f in $TSVS ; do
 	if [[ -n $(grep -E '\s' <<< "$TSVS") ]] ; then
-		echo 'Error: File name should not contain space characters.'
-		echo "Guilty file: $f"
-		exit 1
+		echo 'Error: File name should not contain space characters.' >&2
+		echo "Guilty file: $f" >&2
+		continue
 	fi
 	HEADER=$(head -n1 "$f")	
 	if [[ ! "$HEADER" =~ $HEADER_REGEX ]] ; then
 		echo 'Error: Unexpected or missing table header row' >&2
-		exit 1
+		echo "Guilty file: $f" >&2
+		continue
 	fi
 	tail -n+2 "$f" | sort -o "$f"
 	sed -i "1i $HEADER" "$f"
